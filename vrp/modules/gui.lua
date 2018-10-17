7d326cec6536c1335ae0c7144daeba551855d4fd
Ples Alex <34649178+plesalex100@users.noreply.github.com>
2018-10-17 16:47:01 +0300

    Adds config possibility

--- vrp/modules/gui.lua
+++ vrp/modules/gui.lua
@@ -1,6 +1,6 @@
---- Method 1 ----------------------------------------------
--- EN: Before "function vRP.openMainMenu"
--- RO: Înainte de "function vRP.openMainMenu"
+--1--------------------------------------------------------------------------------------------
+--- EN: At the beggining of the file
+--- RO: La începutul fișierului
 local seq_in = {
   {"cellphone@","cellphone_text_in",1},
   {"cellphone@","cellphone_text_read_base",1}
@@ -11,83 +11,30 @@ local seq_out = {
 local phoneAnim = {}
 AddEventHandler("vRP:playerLeave", function(user_id, source)
   if phoneAnim[source] then
-    TriggerClientEvent("deletePhone", source)
-    vRPclient.playAnim(source,{true,seq_out,false})
+	  TriggerClientEvent("deletePhone", source)
+	  vRPclient.playAnim(source,{true,seq_out,false})
     phoneAnim[source] = false
   end
 end)
-
-
--- EN: At the end of the "vRP.openMainMenu"
--- RO: La sfârșitul funcției "vRP.openMainMenu"
-  if not phoneAnim[source] then
-    TriggerClientEvent("createPhone", source)
-    vRPclient.playAnim(source,{true,seq_in,true})
+-- menus: Menu names cfg to activate animation can be just "Main menu" *case sensitive*
+local menus = {"Phone", "Admin", "menu_name1", "menu_name2"}
+--2--------------------------------------------------------------------------------------------
+--- EN: At the beggining of the function "vRP.openMenu"
+--- RO: La începutul funcției "vRP.openMenu"
+  for i, v in pairs(menus) do
+    if menudef.name == v then
+      if not phoneAnim[source] then
+	      TriggerClientEvent("createPhone", source)
+	      vRPclient.playAnim(source,{true,seq_in,true})
+      end
+      phoneAnim[source] = true
+    end
   end
-  phoneAnim[source] = true
-  
--- EN: In first "if" from function "tvRP.closeMenu"
--- RO: La primul "if" al funcției "tvRP.closeMenu"
+--3--------------------------------------------------------------------------------------------  
+--- EN: In first "if" from function "tvRP.closeMenu"
+--- RO: La primul "if" al funcției "tvRP.closeMenu"
     if phoneAnim[source] then
       TriggerClientEvent("deletePhone", source)
       vRPclient.playAnim(source,{true,seq_out,false})
       phoneAnim[source] = false
     end
-
-
---- Method 2 ----------------------------------------------
--- EN: Replace functions "vRP.openMainMen" and "vRP.openMainMenu"
--- RO: Înlocuiește funcțiile "vRP.openMainMen" și "vRP.openMainMenu"
-
--- MAIN MENU
-
-local seq_in = {
-  {"cellphone@","cellphone_text_in",1},
-  {"cellphone@","cellphone_text_read_base",1}
-}
-local seq_out = {
-  {"cellphone@","cellphone_text_out",1}
-}
-local phoneAnim = {}
-AddEventHandler("vRP:playerLeave", function(user_id, source)
-  if phoneAnim[source] then
-    TriggerClientEvent("deletePhone", source)
-    vRPclient.playAnim(source,{true,seq_out,false})
-    phoneAnim[source] = false
-  end
-end)
-
--- open the player main menu
-function vRP.openMainMenu(source)
-  vRP.buildMenu("main", {player = source}, function(menudata)
-    menudata.name = "Main menu"
-    menudata.css = {top="75px",header_color="rgba(0,125,255,0.75)"}
-    vRP.openMenu(source,menudata) -- open the generated menu
-  end)
-  if not phoneAnim[source] then
-    TriggerClientEvent("createPhone", source)
-    vRPclient.playAnim(source,{true,seq_in,true})
-  end
-  phoneAnim[source] = true
-end
-
--- SERVER TUNNEL API
-
-function tvRP.closeMenu(id)
-  local menu = client_menus[id]
-  if menu and menu.source == source then
-    if phoneAnim[source] then
-      TriggerClientEvent("deletePhone", source)
-      vRPclient.playAnim(source,{true,seq_out,false})
-      phoneAnim[source] = false
-    end
-    -- call callback
-    if menu.def.onclose then
-      menu.def.onclose(source)
-    end
-
-    menu_ids:free(id)
-    client_menus[id] = nil
-    rclient_menus[source] = nil
-  end
-end
